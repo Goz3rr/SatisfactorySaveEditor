c5b154c3f9adaff74fe28b6e78fb28fdbd373f65
virusek20 <gmoder.virusek20@gmail.com>
2019-03-17 02:23:11 +0100

    Add proper localization to node child count

---
+++ SatisfactorySaveEditor/Converter/SaveNodeItemToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SatisfactorySaveEditor.Model;
+
+namespace SatisfactorySaveEditor.Converter
+{
+    public class SaveNodeItemToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is SaveNodeItem saveNodeItem)) return string.Empty;
+
+            var count = saveNodeItem.Items.Count;
+            switch (count)
+            {
+                case 0:
+                    return $"{saveNodeItem.Title}";
+                case 1:
+                    return $"{saveNodeItem.Title} (1 entry)";
+            }
+
+            return $"{saveNodeItem.Title} ({count} entries)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
