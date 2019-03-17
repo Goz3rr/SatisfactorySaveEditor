7309b9174b8e41041e329ba259701f17d7c0406c
virusek20 <gmoder.virusek20@gmail.com>
2019-03-17 15:06:41 +0100

    Add menu bar and file open dialog for picking saves

--- SatisfactorySaveEditor/ViewModel/MainViewModel.cs
+++ SatisfactorySaveEditor/ViewModel/MainViewModel.cs
@@ -1,9 +1,13 @@
+using System;
 using GalaSoft.MvvmLight;
 using SatisfactorySaveEditor.Model;
 using SatisfactorySaveParser;
 using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight.CommandWpf;
 using SatisfactorySaveEditor.Util;
+using System.Windows;
+using Microsoft.Win32;
+using System.Reflection;
 
 namespace SatisfactorySaveEditor.ViewModel
 {
@@ -22,10 +26,62 @@ namespace SatisfactorySaveEditor.ViewModel
 
         public RelayCommand<SaveObjectModel> TreeSelectCommand { get; }
         public RelayCommand<string> JumpCommand { get; }
+        public RelayCommand ExitCommand { get; }
+        public RelayCommand OpenCommand { get; }
+        public RelayCommand AboutCommand { get; }
 
         public MainViewModel()
         {
-            var save = new SatisfactorySave(@"%userprofile%\Documents\My Games\FactoryGame\SaveGame\space war_090319-135233 - Copy.sav");
+            TreeSelectCommand = new RelayCommand<SaveObjectModel>(SelectNode);
+            JumpCommand = new RelayCommand<string>(Jump);
+            ExitCommand = new RelayCommand(Exit);
+            OpenCommand = new RelayCommand(Open);
+            AboutCommand = new RelayCommand(About);
+
+            LoadFile(@"%userprofile%\Documents\My Games\FactoryGame\SaveGame\space war_090319-135233 - Copy.sav");
+        }
+
+        private void About()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            MessageBox.Show($"Satisfactory save editor{Environment.NewLine}{version}", "About");
+        }
+
+        private void Open()
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Satisfactory save file|*.sav",
+                InitialDirectory = Environment.ExpandEnvironmentVariables(@"%userprofile%\Documents\My Games\FactoryGame\SaveGame\")
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                LoadFile(dialog.FileName);
+            }
+        }
+
+        private void Exit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Jump(string target)
+        {
+            SelectedItem.IsSelected = false;
+            SelectedItem = RootItem[0].FindChild(target);
+        }
+
+        private void SelectNode(SaveObjectModel node)
+        {
+            SelectedItem = node;
+        }
+
+        private void LoadFile(string path)
+        {
+            SelectedItem = null;
+
+            var save = new SatisfactorySave(path);
 
             rootItem = new SaveObjectModel("Root");
             var saveTree = new EditorTreeNode("Root");
@@ -38,25 +94,13 @@ namespace SatisfactorySaveEditor.ViewModel
 
             BuildNode(rootItem.Items, saveTree);
 
-            TreeSelectCommand = new RelayCommand<SaveObjectModel>(SelectNode);
-            JumpCommand = new RelayCommand<string>(Jump);
-
             RootItem[0].IsExpanded = true;
             foreach (var item in RootItem[0].Items)
             {
                 item.IsExpanded = true;
             }
-        }
 
-        private void Jump(string target)
-        {
-            SelectedItem.IsSelected = false;
-            SelectedItem = RootItem[0].FindChild(target);
-        }
-
-        private void SelectNode(SaveObjectModel node)
-        {
-            SelectedItem = node;
+            RaisePropertyChanged(() => RootItem);
         }
 
         private void BuildNode(ObservableCollection<SaveObjectModel> items, EditorTreeNode node)
