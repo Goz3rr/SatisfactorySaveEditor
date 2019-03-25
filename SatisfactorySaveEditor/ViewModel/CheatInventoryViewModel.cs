5e543d76bd6d5eca08312d53523fa1ba9b77412b
Rob <budak7273@students.d211.org>
2019-03-25 17:44:56 -0500

    Make progress on custom inventory cheat entering popup. Stuck on `obj.Close();` saying that obj is null

---
+++ SatisfactorySaveEditor/ViewModel/CheatInventoryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using SatisfactorySaveEditor.Model;
+using SatisfactorySaveParser.PropertyTypes;
+
+namespace SatisfactorySaveEditor.ViewModel
+{
+    class CheatInventoryViewModel : ViewModelBase
+    {
+        public RelayCommand<Window> OkCommand { get; }
+        public RelayCommand<Window> CancelCommand { get; }
+        public int NumberChosen { get; set; } = int.MinValue; //default value in case the prompt box is x'd
+
+        public CheatInventoryViewModel()
+        {
+            OkCommand = new RelayCommand<Window>(Ok);
+            CancelCommand = new RelayCommand<Window>(Cancel);
+        }
+
+        private void Cancel(Window obj)
+        {
+            NumberChosen = int.MinValue;
+            obj.Close();
+        }
+
+        private void Ok(Window obj)
+        {
+            MessageBox.Show("Test " + NumberChosen);
+
+            obj.Close();
+        }
+
+    }
+
+
+}
