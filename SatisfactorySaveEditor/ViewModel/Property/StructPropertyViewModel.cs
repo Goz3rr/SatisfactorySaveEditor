088336ad34eef9e989201e53d1eb661b68a1a261
virusek20 <gmoder.virusek20@gmail.com>
2019-04-01 15:42:30 +0200

    Add dynamic struct UI
    Inlcudes custom UI for Vector, Quat, Rotator
    Rest unfinished

--- SatisfactorySaveEditor/ViewModel/Property/StructPropertyViewModel.cs
+++ SatisfactorySaveEditor/ViewModel/Property/StructPropertyViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
-using SatisfactorySaveEditor.Util;
+﻿using SatisfactorySaveEditor.ViewModel.Struct;
 using SatisfactorySaveParser.PropertyTypes;
 using SatisfactorySaveParser.PropertyTypes.Structs;
 
@@ -10,24 +8,28 @@ namespace SatisfactorySaveEditor.ViewModel.Property
     {
         private readonly StructProperty model;
 
+        private object structData; // TODO: Rest of the owl, implement view models for structs
+
         public string Type => model.Type;
 
-        public ObservableCollection<SerializedPropertyViewModel> Fields { get; }
+        public object StructData
+        {
+            get => structData;
+            set { Set(() => StructData, ref structData, value); }
+        }
 
         public StructPropertyViewModel(StructProperty structProperty) : base(structProperty)
         {
             model = structProperty;
 
-            if (model.Data is DynamicStructData dsd)
-            {
-                Fields = new ObservableCollection<SerializedPropertyViewModel>(dsd.Fields.Select(PropertyViewModelMapper.Convert));
-            }
-            else Fields = new ObservableCollection<SerializedPropertyViewModel>();
+            if (model.Data is DynamicStructData dsd) structData = new DynamicStructDataViewModel(dsd);
+            else structData = model.Data;
         }
 
         public override void ApplyChanges()
         {
-            foreach (var field in Fields) field.ApplyChanges();
+            if (structData is DynamicStructDataViewModel dsdvm) dsdvm.ApplyChanges();
+            else model.Data = (IStructData) structData;
         }
     }
 }
