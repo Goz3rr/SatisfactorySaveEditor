8aaba9e5103fda49d7c0c20cd6ae069412420e4a
virusek20 <gmoder.virusek20@gmail.com>
2019-04-03 18:03:52 +0200

    Add basic color editing controls
    Local man spends 4 hours writing a single line solution

---
+++ SatisfactorySaveEditor/View/Control/LinearColorPicker.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using SatisfactorySaveParser.PropertyTypes.Structs;
+
+namespace SatisfactorySaveEditor.View.Control
+{
+    /// <summary>
+    /// Interakční logika pro ColorPicker.xaml
+    /// </summary>
+    public partial class LinearColorPicker : UserControl
+    {
+        public LinearColorPicker()
+        {
+            InitializeComponent();
+        }
+
+        public LinearColor LinearColor
+        {
+            get => (LinearColor)GetValue(LinearColorProperty);
+            set => SetValue(LinearColorProperty, value);
+        }
+
+        public static readonly DependencyProperty LinearColorProperty = DependencyProperty.Register("LinearColor", typeof(LinearColor), typeof(LinearColorPicker));
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            PreviewRectangle.GetBindingExpression(Shape.FillProperty)?.UpdateTarget();
+        }
+    }
+}
