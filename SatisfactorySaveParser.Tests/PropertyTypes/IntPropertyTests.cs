a569a64b04ceeab8c583d1eea247e761e476eb9a
Goz3rr <goz3rr@users.noreply.github.com>
2019-04-03 22:11:42 +0200

    Add IntProperty tests

---
+++ SatisfactorySaveParser.Tests/PropertyTypes/IntPropertyTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SatisfactorySaveParser.PropertyTypes;
+using System.IO;
+
+namespace SatisfactorySaveParser.Tests.PropertyTypes
+{
+    [TestClass]
+    public class IntPropertyTests
+    {
+        private static readonly string IntName = "mNumberOfPassedDays";
+        private static readonly int IntValue = 0;
+        private static readonly byte[] IntBytes = new byte[] { 0x14, 0x00, 0x00, 0x00, 0x6D, 0x4E, 0x75, 0x6D, 0x62, 0x65, 0x72, 0x4F, 0x66, 0x50, 0x61, 0x73, 0x73, 0x65, 0x64, 0x44, 0x61, 0x79, 0x73, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x49, 0x6E, 0x74, 0x50, 0x72, 0x6F, 0x70, 0x65, 0x72, 0x74, 0x79, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+        [TestMethod]
+        public void IntPropertyRead()
+        {
+            using (var stream = new MemoryStream(IntBytes))
+            using (var reader = new BinaryReader(stream))
+            {
+                var prop = SerializedProperty.Parse(reader) as IntProperty;
+
+                Assert.AreNotEqual(null, prop);
+
+                Assert.AreEqual(IntName, prop.PropertyName);
+                Assert.AreEqual(IntProperty.TypeName, prop.PropertyType);
+
+                Assert.AreEqual(0, prop.Index);
+
+                Assert.AreEqual(IntValue, prop.Value);
+
+                Assert.AreEqual(stream.Length, stream.Position);
+            }
+        }
+
+        [TestMethod]
+        public void IntPropertyWrite()
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream))
+            {
+                var prop = new IntProperty(IntName)
+                {
+                    Value = IntValue
+                };
+
+                prop.Serialize(writer);
+
+                Assert.AreEqual(4, prop.SerializedLength);
+                CollectionAssert.AreEqual(IntBytes, stream.ToArray());
+            }
+        }
+    }
+}
