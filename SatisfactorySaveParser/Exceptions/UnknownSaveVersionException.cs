ed3bbc54f4d95ecd4fe3b6f5e3c6f6071a58f119
Goz3rr <goz3rr@users.noreply.github.com>
2019-04-04 21:42:12 +0200

    Switch to new header parsing code

---
+++ SatisfactorySaveParser/Exceptions/UnknownSaveVersionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SatisfactorySaveParser.Exceptions
+{
+    public class UnknownSaveVersionException : Exception
+    {
+        public int SaveVersion { get; set; }
+
+        public UnknownSaveVersionException(int saveVersion)
+        {
+            SaveVersion = saveVersion;
+        }
+    }
+}
--- SatisfactorySaveParser/Save/SaveHeader.cs
+++ SatisfactorySaveParser/Save/SaveHeader.cs
@@ -1,20 +1,49 @@
-﻿using System.IO;
+﻿using SatisfactorySaveParser.Exceptions;
+using System.IO;
 
 namespace SatisfactorySaveParser.Save
 {
     public class SaveHeader
     {
-        public int SaveVersion { get; set; }
-        public int BuildVersion { get; set; }
+        public const int ExpectedMagic = 66297;
 
+        /// <summary>
+        ///     Save version number
+        /// </summary>
+        public int SaveVersion { get; set; }
+        /// <summary>
+        ///     Save build (feature) number
+        /// </summary>
+        public FSaveCustomVersion BuildVersion { get; set; }
+
+        /// <summary>
+        ///     Unknown magic int
+        ///     Seems to always be 66297
+        /// </summary>
         public int Magic { get; set; }
-        
+
+        /// <summary>
+        ///     The name of what appears to be the root object of the save.
+        ///     Seems to always be "Persistent_Level"
+        /// </summary>
         public string MapName { get; set; }
+        /// <summary>
+        ///     An URL style list of arguments of the session.
+        ///     Contains the startloc, sessionName and Visibility
+        /// </summary>
         public string MapOptions { get; set; }
+        /// <summary>
+        ///     Name of the saved game as entered when creating a new game
+        /// </summary>
         public string SessionName { get; set; }
 
+        /// <summary>
+        ///     Amount of seconds spent in this save
+        /// </summary>
         public int PlayDuration { get; set; }
-
+        /// <summary>
+        ///     Unix timestamp of when the save was saved
+        /// </summary>
         public long SaveDateTime { get; set; }
 
         public ESessionVisibility SessionVisibility { get; set; }
@@ -22,7 +51,7 @@ namespace SatisfactorySaveParser.Save
         public void Serialize(BinaryWriter writer)
         {
             writer.Write(SaveVersion);
-            writer.Write(BuildVersion);
+            writer.Write((int)BuildVersion);
             writer.Write(Magic);
 
             writer.WriteLengthPrefixedString(MapName);
@@ -32,7 +61,7 @@ namespace SatisfactorySaveParser.Save
             writer.Write(PlayDuration);
             writer.Write(SaveDateTime);
 
-            if(SaveVersion >= 5)
+            if (SaveVersion >= 5)
                 writer.Write((byte)SessionVisibility);
         }
 
@@ -41,7 +70,7 @@ namespace SatisfactorySaveParser.Save
             var header = new SaveHeader
             {
                 SaveVersion = reader.ReadInt32(),
-                BuildVersion = reader.ReadInt32(),
+                BuildVersion = (FSaveCustomVersion)reader.ReadInt32(),
                 Magic = reader.ReadInt32(),
 
                 MapName = reader.ReadLengthPrefixedString(),
@@ -49,8 +78,17 @@ namespace SatisfactorySaveParser.Save
                 SessionName = reader.ReadLengthPrefixedString(),
 
                 PlayDuration = reader.ReadInt32(),
-                SaveDateTime = reader.ReadInt64(),
+                SaveDateTime = reader.ReadInt64()
             };
+
+            if (header.SaveVersion < 4 || header.SaveVersion > 5)
+                throw new UnknownSaveVersionException(header.SaveVersion);
+
+            if (header.BuildVersion < FSaveCustomVersion.WireSpanFromConnnectionComponents || header.BuildVersion > FSaveCustomVersion.LatestVersion)
+                throw new UnknownBuildVersionException(header.BuildVersion);
+
+            if (header.Magic != ExpectedMagic)
+                throw new FatalSaveException($"Read magic header byte {header.Magic} but {ExpectedMagic} was expected");
 
             if (header.SaveVersion >= 5)
                 header.SessionVisibility = (ESessionVisibility)reader.ReadByte();
