2aacc0dcbc712f9844cabd8f658152550a56c268
virusek20 <gmoder.virusek20@gmail.com>
2019-04-06 01:13:13 +0200

    Validate color data in UI
    Closes #42
    No need to control the byte version, WPF does that automatically since we're using the whole range

--- SatisfactorySaveEditor/Util/FloatRangeRule.cs
+++ SatisfactorySaveEditor/Util/FloatRangeRule.cs
@@ -4,23 +4,27 @@ using System.Windows.Controls;
 
 namespace SatisfactorySaveEditor.Util
 {
-    public class PositiveIntegerRule : ValidationRule
+    public class FloatRangeRule : ValidationRule
     {
+        public float MinValue { get; set; }
+        public float MaxValue { get; set; }
+
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            var number = 0;
+            var number = 0f;
             if (!(value is string numberString)) return new ValidationResult(false, "Input value is not a string.");
+            if (numberString.EndsWith(cultureInfo.NumberFormat.NumberDecimalSeparator)) return new ValidationResult(false, "Input value cannot end with a decimal separator.");
 
             try
             {
-                if (numberString.Length > 0) number = int.Parse(numberString, cultureInfo);
+                if (numberString.Length > 0) number = float.Parse(numberString, cultureInfo);
             }
             catch (Exception e)
             {
                 return new ValidationResult(false, "Illegal characters or " + e.Message);
             }
 
-            if (number < 0) return new ValidationResult(false,"Number is not in range: 0 - Infinity.");
+            if (number > MaxValue || number < MinValue) return new ValidationResult(false,$"Number is not in range: {MinValue} - {MaxValue}.");
             return ValidationResult.ValidResult;
         }
     }
