f69ec203ba114c7470a7679ea09d86a3fc45db87
virusek20 <gmoder.virusek20@gmail.com>
2019-04-05 23:34:32 +0200

    Fix WPF treating underscores as shortcuts in recent files

---
+++ SatisfactorySaveEditor/Converter/MenuTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SatisfactorySaveEditor.Converter
+{
+    public class MenuTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString().Replace("_", "__") ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
