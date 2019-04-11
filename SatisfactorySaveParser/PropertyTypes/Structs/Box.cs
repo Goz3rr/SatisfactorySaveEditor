7fa90b10e9c93f6f6b5f645516ac03fa01b8919c
Goz3rr <goz3rr@users.noreply.github.com>
2019-04-11 16:47:05 +0200

    Make box struct use Vector3

--- SatisfactorySaveParser/PropertyTypes/Structs/Box.cs
+++ SatisfactorySaveParser/PropertyTypes/Structs/Box.cs
@@ -1,16 +1,14 @@
-﻿using System;
+﻿using SatisfactorySaveParser.Structures;
+using System;
 using System.IO;
 
 namespace SatisfactorySaveParser.PropertyTypes.Structs
 {
     public class Box : IStructData
     {
-        public float X1 { get; set; }
-        public float X2 { get; set; }
-        public float Y1 { get; set; }
-        public float Y2 { get; set; }
-        public float Z1 { get; set; }
-        public float Z2 { get; set; }
+        public Vector3 Min { get; set; }
+        public Vector3 Max { get; set; }
+
         public byte UnknownByte { get; set; }
 
         public int SerializedLength => 25;
@@ -18,25 +16,16 @@ namespace SatisfactorySaveParser.PropertyTypes.Structs
 
         public Box(BinaryReader reader)
         {
-            X1 = reader.ReadSingle();
-            Y1 = reader.ReadSingle();
-            Z1 = reader.ReadSingle();
-
-            X2 = reader.ReadSingle();
-            Y2 = reader.ReadSingle();
-            Z2 = reader.ReadSingle();
+            Min = reader.ReadVector3();
+            Max = reader.ReadVector3();
 
             UnknownByte = reader.ReadByte();
         }
 
         public void Serialize(BinaryWriter writer)
         {
-            writer.Write(X1);
-            writer.Write(Y1);
-            writer.Write(Z1);
-            writer.Write(X2);
-            writer.Write(Y2);
-            writer.Write(Z2);
+            writer.Write(Min);
+            writer.Write(Max);
             writer.Write(UnknownByte);
         }
     }
