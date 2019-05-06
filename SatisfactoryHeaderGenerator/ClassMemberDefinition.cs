d816c2ce581dde54da1e0c84d3c0a8f73d9309bd
Goz3rr <goz3rr@users.noreply.github.com>
2019-05-06 17:34:11 +0200

    Start on code generator

---
+++ SatisfactoryHeaderGenerator/ClassMemberDefinition.cs
@@ -0,0 +1,10 @@
+﻿namespace SatisfactoryHeaderGenerator
+{
+    public class ClassMemberDefinition
+    {
+        public string Comment { get; set; }
+        public string Type { get; set; }
+        public string Subtype { get; set; }
+        public string Name { get; set; }
+    }
+}
