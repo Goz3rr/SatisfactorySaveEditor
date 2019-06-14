8eb90c8a91b690ee700295045f8ce59767dd7179
Rob B <computerguy440@gmail.com>
2019-06-13 20:31:12 -0500

    Work on creating 'fake' exe to redirect people that download the source code expecting it to be the release to the correct place

---
+++ SourceCodeMessage/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+using System;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace SourceCodeMessage.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+        
+        public static void Cleanup()
+        {
+        }
+    }
+}
