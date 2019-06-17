2eed26edd2d76c371c4a3ad23a9ac7d316f024a2
Rob B <computerguy440@gmail.com>
2019-06-17 01:47:36 -0500

    Work on generic window to prompt for string values

--- SatisfactorySaveEditor/ViewModel/ViewModelLocator.cs
+++ SatisfactorySaveEditor/ViewModel/ViewModelLocator.cs
@@ -11,8 +11,10 @@ namespace SatisfactorySaveEditor.ViewModel
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
             SimpleIoc.Default.Register<MainViewModel>();
+
             SimpleIoc.Default.Register<AddViewModel>();
             SimpleIoc.Default.Register<CheatInventoryViewModel>();
+            SimpleIoc.Default.Register<StringPromptViewModel>();
             SimpleIoc.Default.Register<PreferencesWindowViewModel>();
         }
 
@@ -21,6 +23,7 @@ namespace SatisfactorySaveEditor.ViewModel
         // The new GUID makes sure we get a fresh instance every time, these windows don't have any persistent data
         public AddViewModel AddViewModel => ServiceLocator.Current.GetInstance<AddViewModel>(Guid.NewGuid().ToString());
         public CheatInventoryViewModel CheatInventoryViewModel => ServiceLocator.Current.GetInstance<CheatInventoryViewModel>(Guid.NewGuid().ToString());
+        public StringPromptViewModel StringPromptViewModel => ServiceLocator.Current.GetInstance<StringPromptViewModel>(Guid.NewGuid().ToString());
         public PreferencesWindowViewModel PreferencesWindowViewModel => ServiceLocator.Current.GetInstance<PreferencesWindowViewModel>(Guid.NewGuid().ToString());
 
         public static void Cleanup()
