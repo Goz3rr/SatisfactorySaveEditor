03b9a9ddf86f193b98b24b3c195d73799fc79807
Rob B <computerguy440@gmail.com>
2019-06-17 01:47:36 -0500

    Work on generic window to prompt for string values

---
+++ SatisfactorySaveEditor/View/StringPromptWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SatisfactorySaveEditor.View
+{
+    /// <summary>
+    /// Interaction logic for CheatInventoryWindow.xaml
+    /// </summary>
+    public partial class CheatInventoryWindow : Window
+    {
+        public CheatInventoryWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
