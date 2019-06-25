9b1bf862a939e6676f62650974f7a021a9c894e0
Goz3rr <goz3rr@users.noreply.github.com>
2019-06-25 22:45:37 +0200

    Update for new experimental

--- SatisfactorySaveParser/Save/SaveHeaderVersion.cs
+++ SatisfactorySaveParser/Save/SaveHeaderVersion.cs
@@ -20,8 +20,8 @@
         // @2019-01-15 Added session visibility to the header so we can set it up with the same visibility
         AddedSessionVisibility,
 
-        // @2019-06-19 Changed how trains are saved.
-        NewRailroadSubsystem,
+        // @2019-06-19 This was put in the wrong save version thingy and is now on experimental so cant remnove it.
+        LookAtTheComment,
 
         // -----<new versions can be added above this line>-----
         VersionPlusOne,
