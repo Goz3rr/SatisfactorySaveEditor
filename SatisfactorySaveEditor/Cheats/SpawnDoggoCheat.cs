b0f449dea6842a209b80d3eabcb560a5b2d84538
Rob B <computerguy440@gmail.com>
2019-08-22 16:45:57 -0500

    Configurable spawn count via wrangling StringPromptWindow because I don't want to write a numeric one.

--- SatisfactorySaveEditor/Cheats/SpawnDoggoCheat.cs
+++ SatisfactorySaveEditor/Cheats/SpawnDoggoCheat.cs
@@ -1,4 +1,6 @@
 ﻿using SatisfactorySaveEditor.Model;
+using SatisfactorySaveEditor.View;
+using SatisfactorySaveEditor.ViewModel;
 using SatisfactorySaveParser;
 using SatisfactorySaveParser.PropertyTypes;
 using SatisfactorySaveParser.PropertyTypes.Structs;
@@ -15,7 +17,7 @@ namespace SatisfactorySaveEditor.Cheats
 {
     class SpawnDoggoCheat : ICheat
     {
-        public string Name => "Spawn Doggo at Host Player";
+        public string Name => "Spawn Doggos...";
 
         private DeleteEnemiesCheat deleteEnemiesCheat; //uses the add doggo code from delete enemies to avoid duplicating code
 
@@ -26,9 +28,58 @@ namespace SatisfactorySaveEditor.Cheats
         
         public bool Apply(SaveObjectModel rootItem)
         {
-            deleteEnemiesCheat.AddDoggo(rootItem);
-            MessageBox.Show("Spawned 1 tamed Lizard Doggo at the host player.");
-            return true;
+            int doggocount = 1;
+
+            var dialog = new StringPromptWindow
+            {
+                Owner = Application.Current.MainWindow
+            };
+            var cvm = (StringPromptViewModel)dialog.DataContext;
+            cvm.WindowTitle = "Enter doggo count";
+            cvm.PromptMessage = "Count (integer):";
+            cvm.ValueChosen = "1";
+            cvm.OldValueMessage = "";
+            dialog.ShowDialog();
+
+            try
+            {
+                doggocount = int.Parse(cvm.ValueChosen);
+                //MessageBox.Show("" + doggocount);
+
+                if (doggocount > 0)
+                {
+                    int counter;
+                    bool pastSuccess = true; //don't keep running the loop if one run fails
+                    for (counter = 0; counter < doggocount && pastSuccess; counter++)
+                    {
+                        pastSuccess = deleteEnemiesCheat.AddDoggo(rootItem);
+                    }
+
+                    if (pastSuccess)
+                    {
+                        MessageBox.Show("Spawned " + counter + " doggo(s) at the host player.");
+                        return true;
+                    }
+                    else
+                    {
+                        //failed to spawn some doggos for some reason
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You can't spawn " + doggocount + " doggos.");
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                if (!(cvm.ValueChosen == "cancel"))
+                {
+                    MessageBox.Show("Could not parse: " + cvm.ValueChosen);
+                }
+                return false;
+            }
         }
         
     }
