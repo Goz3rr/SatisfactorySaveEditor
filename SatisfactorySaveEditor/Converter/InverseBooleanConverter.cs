6fc4bafa39845a260839390682876a17264221fb
Rob B <computerguy440@gmail.com>
2019-08-26 14:25:39 -0500

    Disable menu bar when busy

---
+++ SatisfactorySaveEditor/Converter/InverseBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SatisfactorySaveEditor.Converter
+{
+    //from https://stackoverflow.com/questions/1039636/how-to-bind-inverse-boolean-properties-in-wpf
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
