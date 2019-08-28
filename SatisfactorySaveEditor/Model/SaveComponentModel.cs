ce3179c93bd0c151d565d946b65c7d14c6f262b9
oMaN <orod41285@gmail.com>
2019-08-28 11:24:04 -0400

    Add inventory fill & empty

--- SatisfactorySaveEditor/Model/SaveComponentModel.cs
+++ SatisfactorySaveEditor/Model/SaveComponentModel.cs
@@ -1,4 +1,11 @@
-﻿using SatisfactorySaveParser;
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Command;
+using SatisfactorySaveEditor.View;
+using SatisfactorySaveEditor.ViewModel;
+using SatisfactorySaveEditor.ViewModel.Property;
+using SatisfactorySaveEditor.ViewModel.Struct;
+using SatisfactorySaveParser;
+using SatisfactorySaveParser.PropertyTypes.Structs;
 
 namespace SatisfactorySaveEditor.Model
 {
@@ -12,6 +19,10 @@ namespace SatisfactorySaveEditor.Model
             set { Set(() => ParentEntityName, ref parentEntityName, value); }
         }
 
+        public RelayCommand FillInventoryCommand => new RelayCommand(FillInventory);
+
+        public RelayCommand EmptyInventoryCommand => new RelayCommand(EmptyInventory);
+
         public SaveComponentModel(SaveComponent sc) : base(sc)
         {
             ParentEntityName = sc.ParentEntityName;
@@ -24,6 +35,40 @@ namespace SatisfactorySaveEditor.Model
             var model = (SaveComponent) Model;
 
             model.ParentEntityName = ParentEntityName;
+        }
+
+        private void FillInventory()
+        {
+            FillWindow dialog = new FillWindow();
+            FillViewModel fvm = (FillViewModel) dialog.DataContext;
+            dialog.ShowDialog();
+
+            if(!fvm.IsConfirmed) return;
+            ArrayPropertyViewModel inv = FindField<ArrayPropertyViewModel>("mInventoryStacks");
+            foreach (StructPropertyViewModel element in inv.Elements)
+            {
+                DynamicStructDataViewModel structData = (DynamicStructDataViewModel)element.StructData;
+                InventoryItem item = (InventoryItem)((StructPropertyViewModel)structData.Fields[0]).StructData;
+                IntPropertyViewModel numItems = (IntPropertyViewModel)structData.Fields[1];
+                item.ItemType = fvm.SelectedItem.ItemPath;
+                numItems.Value = fvm.SelectedItem.Quantity;
+            }
+            ApplyChanges();
+        }
+
+        private void EmptyInventory()
+        {
+            var inv = FindField<ArrayPropertyViewModel>("mInventoryStacks");
+            foreach (StructPropertyViewModel element in inv.Elements)
+            {
+                DynamicStructDataViewModel structData = (DynamicStructDataViewModel)element.StructData;
+                InventoryItem item = (InventoryItem)((StructPropertyViewModel)structData.Fields[0]).StructData;
+                IntPropertyViewModel numItems = (IntPropertyViewModel)structData.Fields[1];
+                item.ItemType = string.Empty;
+                numItems.Value = 0;
+            }
+            ApplyChanges();
+            MessageBox.Show($"Inventory for storage {Title} emptied", "Inventory Emptied");
         }
     }
 }
