b7203e7ee871f8dabb3884ea66e8b035f7061743
Rob B <computerguy440@gmail.com>
2020-01-07 20:55:33 -0500

    Update scripts to inform user where they can install strings if they don't have an implementation installed

--- Reference Materials/ExtractResources.ps1
+++ Reference Materials/ExtractResources.ps1
@@ -1,17 +1,32 @@
 "Exporting resources to file. This can take a really long time. A beep will play when it is finished."
 
-if([System.IO.File]::Exists(".\FactoryGame-WindowsNoEditor.pak"))
+Function CommandExists
 {
-	#command that does not save to file, instead printing it to the console
-	#strings -n 30 .\FactoryGame-WindowsNoEditor.pak | Select-String -Pattern "^BlueprintGeneratedClass'/Game/FactoryGame/Resource" | % { "$_".replace("BlueprintGeneratedClass'", "") } | % { "$_".replace("'", "")} 
+	#Needs to be done by a function so the script can still print its error message
+	Param ($command)
+	try {if(Get-Command $command) {RETURN $true}}
+	catch {RETURN $false}
+}
 
-	strings -n 30 .\FactoryGame-WindowsNoEditor.pak | Select-String -Pattern "^BlueprintGeneratedClass'/Game/FactoryGame/Resource" | % { "$_".replace("BlueprintGeneratedClass'", "") } | % { "$_".replace("'", "")} | Add-Content .\Resources.txt
-	[console]::beep(500,125)
-	"Done."
+if(CommandExists strings)
+{
+	if([System.IO.File]::Exists(".\FactoryGame-WindowsNoEditor.pak"))
+	{
+		#command that does not save to file, instead printing it to the console
+		#strings -n 30 .\FactoryGame-WindowsNoEditor.pak | Select-String -Pattern "^BlueprintGeneratedClass'/Game/FactoryGame/Resource" | % { "$_".replace("BlueprintGeneratedClass'", "") } | % { "$_".replace("'", "")} 
+
+		strings -n 30 .\FactoryGame-WindowsNoEditor.pak | Select-String -Pattern "^BlueprintGeneratedClass'/Game/FactoryGame/Resource" | % { "$_".replace("BlueprintGeneratedClass'", "") } | % { "$_".replace("'", "")} | Add-Content .\Resources.txt
+		[console]::beep(500,125)
+		"Done."
+	}
+	else
+	{
+		Write-Host "Couldn't find FactoryGame-WindowsNoEditor.pak`nAre you sure you copied this script into your \SatisfactoryEarlyAccess\FactoryGame\Content\Paks folder?" -ForegroundColor Red
+	}
 }
 else
 {
-	Write-Host "Couldn't find FactoryGame-WindowsNoEditor.pak\nAre you sure you copied this script into your \SatisfactoryEarlyAccess\FactoryGame\Content\Paks folder?" -ForegroundColor Red
+	Write-Host "You don't have an implementation of 'strings' installed. Please install one, such as the one linked here: `nhttps://docs.microsoft.com/en-us/sysinternals/downloads/strings`nYou can install this implementation by putting the exe in your system32 folder, or somewhere else added to PATH" -ForegroundColor Red
 }
 
 pause
