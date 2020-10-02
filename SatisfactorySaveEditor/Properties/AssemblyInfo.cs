412ca3a6d686458531056a437234d7222d30b7ef
Goz3rr <goz3rr@users.noreply.github.com>
2020-10-02 14:20:04 +0200

    bump version to 0.9.10

--- SatisfactorySaveEditor/Properties/AssemblyInfo.cs
+++ SatisfactorySaveEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SatisfactorySaveEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9")]
-[assembly: AssemblyFileVersion("0.9.9")]
+[assembly: AssemblyVersion("0.9.10")]
+[assembly: AssemblyFileVersion("0.9.10")]
