9596c03120735630fdb6ea4bdfcbe8a4c9621b1f
Goz3rr <goz3rr@users.noreply.github.com>
2021-01-04 19:18:12 +0100

    Fix binding error for Vector structs

---
+++ SatisfactorySaveEditor/View/Control/Vector2Control.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Windows;
+using System.Windows.Controls;
+using SatisfactorySaveParser.Structures;
+
+namespace SatisfactorySaveEditor.View.Control
+{
+    public partial class Vector2Control : UserControl
+    {
+        public Vector2Control()
+        {
+            InitializeComponent();
+        }
+
+        public Vector2 Vector
+        {
+            get => (Vector2)GetValue(VectorProperty);
+            set => SetValue(VectorProperty, value);
+        }
+
+        public static readonly DependencyProperty VectorProperty = DependencyProperty.Register("Vector", typeof(Vector2), typeof(Vector2Control));
+    }
+}
