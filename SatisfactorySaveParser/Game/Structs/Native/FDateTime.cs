56a2c00cabca9f847e87f34c145557bd164ce9c0
Goz3rr <goz3rr@users.noreply.github.com>
2021-08-18 22:56:46 +0200

    Add DateTime struct

---
+++ SatisfactorySaveParser/Game/Structs/Native/FDateTime.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace SatisfactorySaveParser.Game.Structs.Native
+{
+    [GameStruct("DateTime")]
+    public class FDateTime : GameStruct
+    {
+        public override string StructName => "DateTime";
+        public override int SerializedLength => 8;
+
+        public long Timestamp { get; set; }
+
+        public override void Deserialize(BinaryReader reader, int buildVersion)
+        {
+            Timestamp = reader.ReadInt64();
+        }
+
+        public override void Serialize(BinaryWriter writer)
+        {
+            writer.Write(Timestamp);
+        }
+    }
+}
