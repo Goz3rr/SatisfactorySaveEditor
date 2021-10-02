46bfa876f5a24ac5baa512a66f2405abf0e5d0ab
Goz3rr <goz3rr@users.noreply.github.com>
2021-10-02 11:09:58 +0200

    Add new header version

--- SatisfactorySaveParser/Save/SaveHeaderVersion.cs
+++ SatisfactorySaveParser/Save/SaveHeaderVersion.cs
@@ -29,6 +29,9 @@
         // @2021-03-24 Added Modding properties and support
         AddedModdingParams,
 
+        // @2021-04-15 UE4.26 Engine Upgrade. FEditorObjectVersion Changes occurred
+        UE426EngineUpdate,
+
         // -----<new versions can be added above this line>-----
         VersionPlusOne,
         LatestVersion = VersionPlusOne - 1 // Last version to use
