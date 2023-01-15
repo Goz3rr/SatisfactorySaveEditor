03520e9ec1ecf927b5e9cb3a23109c07ee067fde
SCHECTER\Freddi <erhardt.frederik@web.de>
2023-01-15 16:35:42 +0100

    add savelevel object class

---
+++ SatisfactorySaveParser/SaveLevel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using SatisfactorySaveParser.Structures;
+
+namespace SatisfactorySaveParser
+{
+    public class SaveLevel
+    {
+        /// <summary>
+        /// The name of the level
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// a list of object instance names that belong in this level.
+        /// </summary>
+        public List<string> ContainedObjectInstances = new List<string>();
+        
+        
+        /// <summary>
+        /// a list of references to collected objects, that belong in this level
+        /// </summary>
+        public List<ObjectReference> ContainedCollectablesInstances = new List<ObjectReference>();
+        
+        public SaveLevel(string name)
+        {
+            Name = name;
+        }
+    }
+}
